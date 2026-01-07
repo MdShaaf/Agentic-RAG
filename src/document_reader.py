453ff3766176b7a46ab26c9c1f61331c4981ca28
Shaaf <mdshafeeq1997@gmail.com>
2026-01-07 18:19:00 +0530

    1st Commit Test

---
+++ src/document_reader.py
@@ -0,0 +1,129 @@
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, CSVLoader, UnstructuredWordDocumentLoader, UnstructuredCSVLoader, Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS, Chroma
+from langchain_openai import OpenAIEmbeddings
+import os
+import pickle
+import hashlib
+import warnings
+warnings.filterwarnings('ignore')
+from sentence_transformers import SentenceTransformer
+import glob
+from pathlib import Path
+from tqdm import tqdm
+from langchain.embeddings.base import Embeddings
+import logging
+
+##Logger settings
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+logging.getLogger("langchain").setLevel(logging.ERROR)
+logging.getLogger("chromadb").setLevel(logging.ERROR)
+logging.getLogger("faiss").setLevel(logging.ERROR)
+
+###Creating Hash for files
+###creating hash for the documents
+
+path = Path(r"C:\Users\Shaaf\Desktop\Data Science\Practice Projects\PDF_Video Reader\PDF_Samples")
+
+def folder_fingerprint(folder_path):
+    hasher = hashlib.sha256()
+
+    for root, _, files in os.walk(folder_path):
+        for file in sorted(files):
+            file_path = os.path.join(root, file)
+            stat = os.stat(file_path)
+            hasher.update(file.encode())
+            hasher.update(str(stat.st_size).encode())
+            hasher.update(str(stat.st_mtime).encode())
+
+    return hasher.hexdigest()
+
+
+import json
+folder_path = path
+FINGERPRINT_FILE = "folder_fingerprint.json"
+
+def load_old_fingerprint():
+    if os.path.exists(FINGERPRINT_FILE):
+        with open(FINGERPRINT_FILE, "r") as f:
+            return json.load(f).get("fingerprint")
+    return None
+
+def save_fingerprint(fp):
+    with open(FINGERPRINT_FILE, "w") as f:
+        json.dump({"fingerprint": fp}, f)
+
+
+documents = []
+
+
+def load_all_documents(path):
+    pdf_files=list(path.rglob("**.pdf"))
+    logger.info(f"Found {len(pdf_files)} PDF files.")
+    for pdf in tqdm(pdf_files, desc="Loading PDF files"):
+        try:
+            loader = PyPDFLoader(str(pdf))
+            docs = loader.load()
+            documents.extend(docs)
+        except Exception as e:
+            logger.error(f"Failed to load PDF {pdf}: {e}")
+
+
+    txt_files = list(path.rglob("**.txt"))
+    logger.info(f"Found {len(txt_files)} TXT files.")
+    for txt_file in tqdm(txt_files, desc="Loading TXT files"):
+        try:
+            loader = TextLoader(str(txt_file))
+            loaded = loader.load()
+            documents.extend(loaded)
+            print(f"[DEBUG] Loaded {len(loaded)} TXT docs from {txt_file}")
+        except Exception as e:
+            logger.error(f"Failed to load TXT {txt_file}: {e}")
+
+
+    docx_files = list(path.rglob("**.docx"))
+    logger.info(f"Found {len(docx_files)} DOCX files")
+    for docx_file in tqdm(docx_files, desc="Loading DOCX files"):
+        try:
+            loader = UnstructuredWordDocumentLoader(str(docx_file))
+            docs = loader.load()
+            documents.extend(docs)
+        except Exception as e:
+            logger.error(f"Failed to load DOCX {docx_file}: {e}")
+
+    csv_files = list(path.rglob("**.csv"))
+    logger.info(f"Found {len(csv_files)} CSV files")
+    for csv_file in tqdm(csv_files, desc="Loading CSV files"):
+        try:
+            loader = UnstructuredCSVLoader(str(csv_file))
+            docs = loader.load()
+            documents.extend(docs)
+        except Exception as e:
+            logger.error(f"Failed to load CSV {csv_file}: {e}")
+
+DOC_CACHE_FILE = "cached_documents.pkl"
+new_folder_fingerprint = folder_fingerprint(folder_path)
+old_folder_fingerprint = load_old_fingerprint()
+
+if new_folder_fingerprint != old_folder_fingerprint or not os.path.exists(DOC_CACHE_FILE):
+    logger.info("Folder contents changed. Reloading documents...")
+    load_all_documents(folder_path)
+    logger.info("Saving documents to cache...")
+    with open(DOC_CACHE_FILE, "wb") as f:
+        pickle.dump(documents, f)
+
+    save_fingerprint(new_folder_fingerprint)
+
+else:
+    logger.info("Folder contents unchanged. Loading documents from cache...")
+    with open(DOC_CACHE_FILE, "rb") as f:
+        documents = pickle.load(f)
+
+    logger.info(f"Total documents loaded: {len(documents)}")
+
